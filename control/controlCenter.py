20d1142fbc775ada1b535910a0448e2607433326
Pawel Kremienowski <Kremienowski33@gmail.com>
2021-11-14 16:45:00 +0100

    python to communicate with arduino

--- control/controlCenter.py
+++ control/controlCenter.py
@@ -1,3 +1,7 @@
+# import zmq
+import serial
+import time
+
 '''
 Created Date: Sunday, November 14th 2021
 Author: Pawel Kremienowski
@@ -7,7 +11,20 @@ Last modified:
 Modified by: Pawel Kremienowski
 '''
 
-print("bye")
 
-print("jo")
-print("hello world")
+arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
+
+
+def sendToArduino(msg: str):
+    arduino.write(bytes(msg, 'utf-8'))
+    time.sleep(0.2)
+
+
+if __name__ == '__main__':
+    try:
+        msg = input("Enter message to Arduino: ")
+        sendToArduino(msg)
+    except KeyboardInterrupt:
+        sendToArduino("diodes off")
+        print("CTRL+C pressed. Finishing")
+        exit()
