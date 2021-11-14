29c0726324ca315c2f9236d288f13bc87abe5ae0
Pawel Kremienowski <Kremienowski33@gmail.com>
2021-11-14 16:14:10 +0100

    arduino controls diodes state

--- arduino/arduinoControl.ino
+++ arduino/arduinoControl.ino
@@ -7,11 +7,66 @@
  * Modified by: Pawel Kremienowski
  */
 
+const int BLUE_DIODE = 2;
+const int RED_DIODE = 3;
+const int YELLOW_DIODE = 4;
+
+const String BLUE_ON = "blue on";
+const String RED_ON = "red on";
+const String YELLOW_ON = "yellow on";
+const String DIODES_OFF = "diodes off";
+
+String received = "";
+
 void setup()
 {
+    Serial.begin(9600);
+    pinMode(BLUE_DIODE, OUTPUT);
+    pinMode(RED_DIODE, OUTPUT);
+    pinMode(YELLOW_DIODE, OUTPUT);
+
+    // turning off diodes at beggining
+    turnAllDiodesOff();
 }
 
 void loop()
 {
-    delay(500);
+    if (Serial.available() > 0)
+    {
+        received = Serial.readStringUntil('\n');
+        diodesAction(received);
+    }
+
+    delay(100);
+}
+
+void diodesAction(String command)
+{
+    if (command.equals(BLUE_ON))
+    {
+        digitalWrite(BLUE_DIODE, HIGH);
+    }
+    else if (command.equals(RED_ON))
+    {
+        digitalWrite(RED_DIODE, HIGH);
+    }
+    else if (command.equals(YELLOW_ON))
+    {
+        digitalWrite(YELLOW_DIODE, HIGH);
+    }
+    else if (command.equals(DIODES_OFF))
+    {
+        turnAllDiodesOff();
+    }
+    else
+    {
+        Serial.println("diodes action NONE");
+    }
+}
+
+void turnAllDiodesOff()
+{
+    digitalWrite(BLUE_DIODE, LOW);
+    digitalWrite(RED_DIODE, LOW);
+    digitalWrite(YELLOW_DIODE, LOW);
 }
